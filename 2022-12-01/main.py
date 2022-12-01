178052e42d41c33384fedb015f95b0cb35379b93
Tim Harrison <gh@tjth.co>
2022-12-01 07:36:21 +0000

    Day #1

---
+++ 2022-12-01/main.py
@@ -0,0 +1,24 @@
+with open("input.txt") as input_file:
+    input_values = input_file.read()
+
+elves = input_values.replace("\n",",").split(",,")[:-1]
+
+totals = []
+for elf in elves:
+    elf = elf.split(",")
+    total = 0
+    for snack in elf:
+        total += int(snack)
+    totals.append(total)
+
+totals.sort()
+
+# Most
+print(totals[-1])
+
+# Most top three 
+top_three = totals[-3:]
+total_top_three = 0
+for pos in top_three:
+    total_top_three += pos
+print(total_top_three)
